07e4b370275a53f39675e12f2776e84b3cc18a3d
Amna <123323721+Amnafazal2004@users.noreply.github.com>
2025-11-28 01:58:04 +0500

    Update Footer.jsx

--- client/Components/Footer.jsx
+++ client/Components/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
    
 
     <>
-      <div id="Contact" className='text-footer bg-grey1 flex-col items-center gap-5 p-5 pt-20 bg-[#3c5e78] ' >
+      <div id="Contact" className='text-footer bg-grey1 flex-col items-center gap-5 p-5 pt-20 bg-[#155076] ' >
        <div className="flex justify-between ">
         <div className="footercontentleft">
            <h1 className='font-bold text-2xl text-white font-[playfair_display] mb-5'>PASTEL THRIFTS</h1>
@@ -49,3 +49,4 @@ const Footer = () => {
 }
 
 export default Footer
+
