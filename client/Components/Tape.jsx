adde22c26f606691efb671802a6c5a6297410d31
Amna <123323721+Amnafazal2004@users.noreply.github.com>
2025-11-28 01:59:59 +0500

    Update Tape.jsx

--- client/Components/Tape.jsx
+++ client/Components/Tape.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Tape = () => {
   return (
     <>
-    <div id="About" className="w-full bg-[#2b5e84] py-3 overflow-hidden">
+    <div id="About" className="w-full bg-[#155076] py-3 overflow-hidden">
   <div className="whitespace-nowrap animate-marquee text-white font-bold uppercase tracking-widest text-[14px]">
     THRIFT WITH PURPOSE • SUSTAINABLE STYLE • ONE-OF-A-KIND FINDS • VINTAGE LOVE • 
     THRIFT WITH PURPOSE • SUSTAINABLE STYLE • ONE-OF-A-KIND FINDS • VINTAGE LOVE • 
@@ -17,3 +17,4 @@ const Tape = () => {
 }
 
 export default Tape
+
