a53ef298be557181f149fc1838248ed9fc2c1b0b
Amna <123323721+Amnafazal2004@users.noreply.github.com>
2025-11-28 09:36:21 +0500

    Update Header.jsx

--- client/Components/Header.jsx
+++ client/Components/Header.jsx
@@ -28,9 +28,9 @@ const Header = () => {
 
         {/* Centered logo + title */}
         <div className="flex py-2 items-center">
-          {/* <Image src={logo1} alt="Clothing Logo" className="w-10 h-10" /> */}
+          <Image src={logo1} alt="Clothing Logo" className="w-10 h-10" /> 
           <div className="text-xl font-bold font-[Playfair_Display] text-black">
-            Pastel Thrift
+            Pastel Thrifts
           </div>
         </div>
 
@@ -104,6 +104,7 @@ const Header = () => {
 }
 
 export default Header
+
 
 
 
